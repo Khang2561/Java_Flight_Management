c5f9f469550db07273e1ffed0733438d93156ffe
taisu1412 <trananhhaoo0603@gmail.com>
2024-05-12 12:06:07 +0700

    create plane part UI

--- Mangement_fight/src/View/Admin/Plane/OperationPlaneUC.java
+++ Mangement_fight/src/View/Admin/Plane/OperationPlaneUC.java
@@ -1,15 +1,139 @@
 package View.Admin.Plane;
 
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class OperationPlaneUC extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField textFieldPlaneName;
+	private JTextField textFieldChairCount;
 
 	/**
 	 * Create the panel.
 	 */
 	public OperationPlaneUC() {
+		setBounds(0, 0, 1365, 520);
+		setLayout(null);
+		
+		JPanel panelLeft = new JPanel();
+		panelLeft.setBounds(10, 10, 554, 500);
+		add(panelLeft);
+		panelLeft.setLayout(new GridLayout(0, 2, 40, 0));
+		
+		JPanel panel = new JPanel();
+		panelLeft.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Thông tin máy bay");
+		lblNewLabel.setBounds(48, 20, 161, 19);
+		lblNewLabel.setLabelFor(panel);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+		panel.add(lblNewLabel);
+		
+		textFieldPlaneName = new JTextField();
+		textFieldPlaneName.setBounds(30, 67, 197, 30);
+		panel.add(textFieldPlaneName);
+		textFieldPlaneName.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Tên máy bay");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2.setBounds(30, 49, 90, 19);
+		panel.add(lblNewLabel_2);
+		
+		textFieldChairCount = new JTextField();
+		textFieldChairCount.setColumns(10);
+		textFieldChairCount.setBounds(30, 126, 197, 30);
+		panel.add(textFieldChairCount);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Số lượng ghế");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2_1.setBounds(30, 107, 90, 19);
+		panel.add(lblNewLabel_2_1);
+		
+		JButton btnNewButton = new JButton("Tải lại danh sách ghế");
+		btnNewButton.setBounds(97, 166, 130, 21);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("Chi tiết hạng vé");
+		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2_1_1.setBounds(30, 197, 114, 19);
+		panel.add(lblNewLabel_2_1_1);
+		
+		JPanel panelChitietve = new JPanel();
+		panelChitietve.setBounds(30, 226, 197, 215);
+		panel.add(panelChitietve);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(30, 451, 197, 39);
+		panel.add(panel_2);
+		panel_2.setLayout(new GridLayout(0, 2, 40, 0));
+		
+		JButton btnLuu = new JButton("Lưu");
+		btnLuu.setFont(new Font("Tahoma", Font.BOLD, 15));
+		panel_2.add(btnLuu);
+		
+		JButton btnHuy = new JButton("Hủy");
+		btnHuy.setFont(new Font("Tahoma", Font.BOLD, 15));
+		panel_2.add(btnHuy);
+		
+		JPanel panel_1 = new JPanel();
+		panelLeft.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Hạng vé");
+		lblNewLabel_1.setBounds(88, 18, 81, 19);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblNewLabel_1.setLabelFor(panel_1);
+		panel_1.add(lblNewLabel_1);
+		
+		JPanel panelHangve = new JPanel();
+		panelHangve.setBounds(30, 56, 197, 385);
+		panel_1.add(panelHangve);
+		
+		JButton btnNewButton_1 = new JButton("Chưa có hạng vé");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton_1.setBounds(30, 451, 197, 39);
+		panel_1.add(btnNewButton_1);
+		
+		JPanel panelRight = new JPanel();
+		panelRight.setBounds(608, 65, 747, 445);
+		add(panelRight);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(608, 39, 747, 26);
+		add(panel_3);
+		panel_3.setLayout(new GridLayout(0, 2, 80, 0));
+		
+		JPanel panel_4 = new JPanel();
+		panel_3.add(panel_4);
+		panel_4.setLayout(new GridLayout(0, 3, 20, 0));
+		
+		JButton btnNewButton_2 = new JButton("1");
+		panel_4.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("2");
+		panel_4.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("3");
+		panel_4.add(btnNewButton_4);
+		
+		JPanel panel_5 = new JPanel();
+		panel_3.add(panel_5);
+		panel_5.setLayout(new GridLayout(0, 3, 20, 0));
+		
+		JButton btnNewButton_7 = new JButton("4");
+		panel_5.add(btnNewButton_7);
+		
+		JButton btnNewButton_6 = new JButton("5");
+		panel_5.add(btnNewButton_6);
+		
+		JButton btnNewButton_5 = new JButton("6");
+		panel_5.add(btnNewButton_5);
 
 	}
 
--- Mangement_fight/src/View/Admin/Plane/PlaneListUC.java
+++ Mangement_fight/src/View/Admin/Plane/PlaneListUC.java
@@ -1,16 +1,80 @@
 package View.Admin.Plane;
 
+import java.awt.EventQueue;
+
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import View.Admin.FormAdmin;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class PlaneListUC extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Create the panel.
-	 */
+	private JTable table;
+	private JTextField textField;
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					//setting value default 
+					
+					/*
+					Parameters settingValue = new Parameters();
+					ResultSet rs = ParametersDAO.selectAll();
+					settingValue.setMinimumFlightTime(rs.getInt("MinimunFlightTime"));
+					settingValue.setMaxPreventiveAirports(rs.getInt("MaxPreventiveAirports"));
+					settingValue.setMinimumStopoverTime(rs.getInt("MinimumStopoverTim"));
+					settingValue.setMaximumStopoverTime(rs.getInt("MaximumStopoverTime"));
+					settingValue.setEarliestBookingTime(rs.getInt("EarliestBookingTime"));
+					settingValue.setLatestBookingCancellationTime(rs.getInt("LatestBookingCancellationTime"));
+					*/
+					//setting value default
+					
+					new PlaneListUC();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 	public PlaneListUC() {
+		setLayout(null);
+		table = new JTable();
+
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Mã máy bay", "Tên máy bay", "Số lượng ghế", "Thao tác"
+			}
+		));
+		table.getColumnModel().getColumn(1).setPreferredWidth(83);
+		
+
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(0, 80, 1365, 440);
+		add(scrollPane);
+		
+		textField = new JTextField();
+		textField.setToolTipText("Mã máy bay hoặc tên máy bay");
+		textField.setBounds(10, 45, 249, 25);
+		add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Nhập mã máy bay hoặc tên máy bay");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(10, 10, 260, 25);
+		add(lblNewLabel);
+		
+
 
 	}
-
 }
